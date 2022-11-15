76823f5d38db23c8c187ead3b7deb03e0c479690
Marcela Ang <marcela.ang@gmail.com>
2022-11-15 12:00:41 -0500

    M1W5 - Snek

--- play.js
+++ play.js
@@ -1,24 +1,6 @@
-const net = require("net");
+const connect = require("./client.js");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: '50541',
 
-  });
-
-  conn.on('data', (data) => {
-  // code that does something when the connection is first established
-      console.log(data);
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-
-};
 
 
 
